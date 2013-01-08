e01ea8385d389924e1ad4a3e454d9a23f92ff7f7
Vitalij <sounder.vitalij@gmail.com>
2013-01-08 17:39:57 +0200

    initial commit

---
+++ InstaGPlus/EXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EXAppDelegate.h
+//  InstaGPlus
+//
+//  Created by MacUser1 on 1/8/13.
+//  Copyright (c) 2013 MacUser1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstaGPlus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InstaGPlus
+//
+//  Created by MacUser1 on 1/8/13.
+//  Copyright (c) 2013 MacUser1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EXAppDelegate class]));
+    }
+}
